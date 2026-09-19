94d8e57e20762ba45b3d3f138b716a69391e9da5
thabisovava12-ctrl <thabisovava12@gmail.com>
2026-09-19 21:57:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask
-import os
+from flask import Flask, send_file
+import os, threading, time
+
 app = Flask(__name__)
+BOT_RUNNING = False
+
+def sr72_logic():
+    while True:
+        if not BOT_RUNNING:
+            time.sleep(2)
+            continue
+        print("SR-72: Checking Gold...")
+        time.sleep(5)
+
 @app.route('/')
 def home():
-    return "<h1>SR72 VPS LIVE ✅</h1>"
+    return send_file('index.html')
+
+@app.route('/start')
+def start():
+    global BOT_RUNNING
+    BOT_RUNNING = True
+    return "STARTED"
+
+@app.route('/stop')
+def stop():
+    global BOT_RUNNING
+    BOT_RUNNING = False
+    return "STOPPED"
+
+threading.Thread(target=sr72_logic, daemon=True).start()
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
