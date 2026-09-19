1bebeffb026f260abbe44a10c35bc614e5de2c69
thabisovava12-ctrl <thabisovava12@gmail.com>
2026-09-19 14:11:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
-from flask import Flask, send_file
-import threading, time
+from flask import Flask
 app = Flask(__name__)
-BOT=False
-def bot_loop():
-    while True:
-        if BOT: print("SR-72 running... XAUUSD straddle check")
-        time.sleep(5)
 @app.route('/')
-def home(): return send_file('index.html')
-@app.route('/start')
-def start(): 
-    global BOT; BOT=True; return "BOT STARTED"
-@app.route('/stop')
-def stop():
-    global BOT; BOT=False; return "BOT STOPPED"
-threading.Thread(target=bot_loop, daemon=True).start()
-app.run(host='0.0.0.0', port=10000)
+def home():
+    return "SR72 VPS is LIVE!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
